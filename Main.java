9152886dfdc2e5bfdd20c813af23bdaa348efa57
serge loie <serge.loie@yandex.ru>
2024-07-05 14:38:36 +0500

    add Main

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import array.LeftAndRightSumDifferences_2574;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+        LeftAndRightSumDifferences_2574 lfm = new LeftAndRightSumDifferences_2574();
+        int[] test = {10,4,8,3};
+        int[] result = lfm.leftRightDifference(test);
+        System.out.println(Arrays.toString(result));
     }
 }
