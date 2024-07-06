0c5bdd5fc81886f06d0a0ca117ff15782fc45911
daBoss <serge.loie@yandex.ru>
2024-07-06 15:20:28 +0500

    gitignore update

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
 import array.LeftAndRightSumDifferences_2574;
+import matrix.ToeplitzMatrix_766;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        LeftAndRightSumDifferences_2574 lfm = new LeftAndRightSumDifferences_2574();
-        int[] test = {10,4,8,3};
-        int[] result = lfm.leftRightDifference(test);
-        System.out.println(Arrays.toString(result));
+        ToeplitzMatrix_766 lfm = new ToeplitzMatrix_766();
+        int[][] test = {{1,2}, {2,2}};
+        boolean result = lfm.isToeplitzMatrix(test);
+        System.out.println(result);
     }
 }
