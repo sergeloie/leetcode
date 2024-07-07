a7c3b9f53272792132141cfbadce20d17c03cef2
daBoss <serge.loie@yandex.ru>
2024-07-07 23:06:57 +0500

    896. Monotonic Array not solved

--- Main.java
+++ Main.java
@@ -1,15 +1,16 @@
 import array.LeftAndRightSumDifferences_2574;
 import array_plus.FindTheHighestAltitude_1732;
 import array_plus.LongestContinuousIncreasingSubsequence_674;
+import array_plus.MonotonicArray_896;
 import matrix.ToeplitzMatrix_766;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        FindTheHighestAltitude_1732 cls = new FindTheHighestAltitude_1732();
-        int[] test = {-4,-3,-2,-1,4,3,2};
-        int result = cls.largestAltitude(test);
+        MonotonicArray_896 cls = new MonotonicArray_896();
+        int[] test = {1,3,2};
+        boolean result = cls.isMonotonic(test);
         System.out.println(result);
     }
 
---
+++ array_plus/MonotonicArray_896.java
@@ -0,0 +1,17 @@
+package array_plus;
+
+public class MonotonicArray_896 {
+    public boolean isMonotonic(int[] nums) {
+        int l = 1;
+        int r = nums.length - 2;
+        while (l < r) {
+            if ((nums[l] >= nums[l - 1] & nums[r + 1] >= nums[r]) ||  (nums[l] <= nums[l - 1] & nums[r + 1] <= nums[r])) {
+                l++;
+                r--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
