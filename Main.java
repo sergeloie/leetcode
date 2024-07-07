cd13d0086dcecc2776146a927373f1a5b3513e06
daBoss <serge.loie@yandex.ru>
2024-07-07 22:45:52 +0500

    1732. Find the Highest Altitude solved

--- Main.java
+++ Main.java
@@ -1,13 +1,16 @@
 import array.LeftAndRightSumDifferences_2574;
+import array_plus.FindTheHighestAltitude_1732;
+import array_plus.LongestContinuousIncreasingSubsequence_674;
 import matrix.ToeplitzMatrix_766;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        ToeplitzMatrix_766 lfm = new ToeplitzMatrix_766();
-        int[][] test = {{1,2}, {2,2}};
-        boolean result = lfm.isToeplitzMatrix(test);
+        FindTheHighestAltitude_1732 cls = new FindTheHighestAltitude_1732();
+        int[] test = {-4,-3,-2,-1,4,3,2};
+        int result = cls.largestAltitude(test);
         System.out.println(result);
     }
+
 }
