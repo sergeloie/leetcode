7778222a695c940c05fc7754d9ac5a2b29570aa1
serge loie <serge.loie@yandex.ru>
2024-07-08 10:07:49 +0500

    896. Monotonic Array solved

--- array_plus/MonotonicArray_896.java
+++ array_plus/MonotonicArray_896.java
@@ -1,14 +1,53 @@
 package array_plus;
+/*
+https://leetcode.com/problems/monotonic-array/description/
+896. Monotonic Array
+Easy
+An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
+
+Given an integer array nums, return true if the given array is monotonic, or false otherwise.
+
+
+
+Example 1:
+
+Input: nums = [1,2,2,3]
+Output: true
+Example 2:
+
+Input: nums = [6,5,4,4]
+Output: true
+Example 3:
+
+Input: nums = [1,3,2]
+Output: false
+ */
 
 public class MonotonicArray_896 {
     public boolean isMonotonic(int[] nums) {
-        int l = 1;
-        int r = nums.length - 2;
-        while (l <= r) {
-            if ((nums[l] >= nums[l - 1] & nums[r + 1] >= nums[r]) ||  (nums[l] <= nums[l - 1] & nums[r + 1] <= nums[r])) {
-                l++;
-                r--;
-            } else {
+        return nums[0] < nums[nums.length - 1] ? isMonotonicInc(nums) : isMonotonicDec(nums);
+    }
+
+    public boolean isMonotonicInc(int[] nums) {
+        if (nums.length <= 1) {
+            return true;
+        }
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isMonotonicDec(int[] nums) {
+        if (nums.length <= 1) {
+            return true;
+        }
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
                 return false;
             }
         }
