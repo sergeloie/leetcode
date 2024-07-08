50753dcff530f530a9ef480aa53dfc40c221b2c9
serge loie <serge.loie@yandex.ru>
2024-07-08 14:26:50 +0500

    941. Valid Mountain Array solved

--- Main.java
+++ Main.java
@@ -2,16 +2,16 @@ import array.LeftAndRightSumDifferences_2574;
 import array_plus.FindTheHighestAltitude_1732;
 import array_plus.LongestContinuousIncreasingSubsequence_674;
 import array_plus.MonotonicArray_896;
+import array_plus.ValidMountainArray_941;
 import matrix.ToeplitzMatrix_766;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        MonotonicArray_896 cls = new MonotonicArray_896();
-        int[] test = {1,3,2};
-        boolean result = cls.isMonotonic(test);
+        ValidMountainArray_941 cls = new ValidMountainArray_941();
+        int[] test = {0,1,2,3,4,5,6,7,8,9};
+        boolean result = cls.validMountainArray(test);
         System.out.println(result);
     }
-
 }
