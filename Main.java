3f3aece163d318387fd43a0f7eaaa9ffa4b8266c
daBoss <serge.loie@yandex.ru>
2024-07-11 22:14:59 +0500

    412. Fizz Buzz solved

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import array_plus.LongestContinuousIncreasingSubsequence_674;
 import array_plus.MonotonicArray_896;
 import array_plus.SummaryRanges_228;
 import array_plus.ValidMountainArray_941;
+import math.FizzBuzz_412;
 import matrix.ToeplitzMatrix_766;
 import strings.ShuffleString_1528;
 
@@ -11,10 +12,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        ShuffleString_1528 cls = new ShuffleString_1528();
-        int[] indices = {4,5,6,7,0,2,1,3};
-        String s = "codeleet";
-        var result = cls.restoreString(s, indices);
+        FizzBuzz_412 cls = new FizzBuzz_412();
+        var result = cls.fizzBuzz(3);
         System.out.println(result);
     }
 }
