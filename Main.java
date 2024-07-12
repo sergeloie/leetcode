e6b1ceb3ee1706dd0f15e16f81af54ba8ca85a3f
serge loie <serge.loie@yandex.ru>
2024-07-12 10:20:52 +0500

    258. Add Digits solved

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import array_plus.LongestContinuousIncreasingSubsequence_674;
 import array_plus.MonotonicArray_896;
 import array_plus.SummaryRanges_228;
 import array_plus.ValidMountainArray_941;
+import math.AddDigits_258;
 import math.FizzBuzz_412;
 import matrix.ToeplitzMatrix_766;
 import strings.ShuffleString_1528;
@@ -12,8 +13,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        FizzBuzz_412 cls = new FizzBuzz_412();
-        var result = cls.fizzBuzz(3);
+        AddDigits_258 cls = new AddDigits_258();
+        var result = cls.addDigits(0);
         System.out.println(result);
     }
 }
