59a3810dad49de54aa018eedf789ae9f60998291
daBoss <serge.loie@yandex.ru>
2024-07-13 19:42:40 +0500

    383. Ransom Note solved

---
+++ hashmap/RansomNote_383.java
@@ -0,0 +1,50 @@
+package hashmap;
+
+/*
+https://leetcode.com/problems/ransom-note/description/
+383. Ransom Note
+Easy
+
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+
+
+Example 1:
+
+Input: ransomNote = "a", magazine = "b"
+Output: false
+
+Example 2:
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+
+Example 3:
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+
+ */
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class RansomNote_383 {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<String, Long> rMap = Arrays.stream(ransomNote.split(""))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        Map<String, Long> mMap = Arrays.stream(magazine.split(""))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        for (Map.Entry<String, Long> entry : rMap.entrySet()) {
+            String s = entry.getKey();
+            if (mMap.get(s) == null || entry.getValue() > mMap.get(s)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
