22c5d35754db4e496d97e22d27f307f6fbe24336
daBoss <serge.loie@yandex.ru>
2024-07-13 16:52:26 +0500

    1684. Count the Number of Consistent Strings solved

---
+++ hashset/CountTheNumberOfConsistentStrings_1684.java
@@ -0,0 +1,48 @@
+package hashset;
+
+/*
+https://leetcode.com/problems/count-the-number-of-consistent-strings/description/
+1684. Count the Number of Consistent Strings
+Easy
+
+You are given a string allowed consisting of distinct characters and an array of strings words. A string is consistent if all characters in the string appear in the string allowed.
+
+Return the number of consistent strings in the array words.
+
+
+
+Example 1:
+
+Input: allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
+Output: 2
+Explanation: Strings "aaab" and "baa" are consistent since they only contain characters 'a' and 'b'.
+
+Example 2:
+
+Input: allowed = "abc", words = ["a","b","c","ab","ac","bc","abc"]
+Output: 7
+Explanation: All strings are consistent.
+
+Example 3:
+
+Input: allowed = "cad", words = ["cc","acd","b","ba","bac","bad","ac","d"]
+Output: 4
+Explanation: Strings "cc", "acd", "ac", and "d" are consistent.
+
+ */
+
+import java.util.List;
+import java.util.Set;
+
+public class CountTheNumberOfConsistentStrings_1684 {
+    public int countConsistentStrings(String allowed, String[] words) {
+        int count = 0;
+        Set<String> set = Set.of(allowed.split(""));
+
+        for (String word : words) {
+            List<String> wordSet = List.of(word.split(""));
+            count = set.containsAll(wordSet) ? count + 1 : count;
+        }
+        return count;
+    }
+}
