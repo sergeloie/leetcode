d419f75c7df4f3fc0654521468bd2fd755229490
serge loie <serge.loie@yandex.ru>
2024-07-14 17:41:01 +0500

    383. Ransom Note begin

---
+++ hashmap/FindWordsThatCanBeFormedByCharacters_1160.java
@@ -0,0 +1,11 @@
+package hashmap;
+
+/*
+https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/
+ */
+
+public class FindWordsThatCanBeFormedByCharacters_1160 {
+    public int countCharacters(String[] words, String chars) {
+
+    }
+}
