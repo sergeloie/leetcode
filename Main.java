f957737ea5c120343e98211924801b89b158f047
serge loie <serge.loie@yandex.ru>
2024-07-14 13:21:40 +0500

    383. Ransom Note faster solved

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
-import hashset.PathCrossing_1496;
+import hashmap.RansomNote_383;
 import math.AddDigits_258;
 
 public class Main {
     public static void main(String[] args) {
-        PathCrossing_1496 cls = new PathCrossing_1496();
-        var result = cls.isPathCrossing("NESWW");
+        RansomNote_383 cls = new RansomNote_383();
+        var result = cls.canConstruct2("aa", "aab");
         System.out.println(result);
     }
 }
