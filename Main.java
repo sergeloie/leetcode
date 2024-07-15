671509bd3221318ae4e2a50184821b1ce86a2e1d
serge loie <serge.loie@yandex.ru>
2024-07-15 17:31:54 +0500

    1. Two Sum begin

--- Main.java
+++ Main.java
@@ -1,13 +1,16 @@
 import hashmap.FindWordsThatCanBeFormedByCharacters_1160;
 import hashmap.RansomNote_383;
+import hashmap.TwoSum_1;
 import math.AddDigits_258;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        FindWordsThatCanBeFormedByCharacters_1160 cls = new FindWordsThatCanBeFormedByCharacters_1160();
-        String[] words = {"hello","world","leetcode"};
-        String chars = "welldonehoneyr";
-        int result = cls.countCharacters(words, chars);
-        System.out.println(result);
+        TwoSum_1 cls = new TwoSum_1();
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        int[] result = cls.twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
     }
 }
