c876950dc95c7ac48522d5cd2ad6725d724f4126
serge loie <serge.loie@yandex.ru>
2024-07-16 09:55:34 +0500

    1365. How Many Numbers Are Smaller Than the Current Number solved

--- Main.java
+++ Main.java
@@ -1,16 +1,11 @@
-import hashmap.FindWordsThatCanBeFormedByCharacters_1160;
 import hashmap.HowManyNumbersAreSmallerThanTheCurrentNumber_1365;
-import hashmap.RansomNote_383;
-import hashmap.TwoSum_1;
-import math.AddDigits_258;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         HowManyNumbersAreSmallerThanTheCurrentNumber_1365 cls = new HowManyNumbersAreSmallerThanTheCurrentNumber_1365();
-        int[] nums = {8,1,2,2,3};
-        int target = 9;
+        int[] nums = {60,14,3,39,49,43,53,24,33,13,32,93,65,26,77,55,2,28,2,50,18,4,92,20,57,90,64,86,54,69,28,80,88,66,57,28,67,83,3,50,86};
         int[] result = cls.smallerNumbersThanCurrent(nums);
         System.out.println(Arrays.toString(result));
     }
--- hashmap/HowManyNumbersAreSmallerThanTheCurrentNumber_1365.java
+++ hashmap/HowManyNumbersAreSmallerThanTheCurrentNumber_1365.java
@@ -2,6 +2,33 @@ package hashmap;
 
 /*
 https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/description/
+1365. How Many Numbers Are Smaller Than the Current Number
+Easy
+Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+Return the answer in an array.
+
+
+
+Example 1:
+
+Input: nums = [8,1,2,2,3]
+Output: [4,0,1,1,3]
+Explanation:
+For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3).
+For nums[1]=1 does not exist any smaller number than it.
+For nums[2]=2 there exist one smaller number than it (1).
+For nums[3]=2 there exist one smaller number than it (1).
+For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+Example 2:
+
+Input: nums = [6,5,4,8]
+Output: [2,1,0,3]
+Example 3:
+
+Input: nums = [7,7,7,7]
+Output: [0,0,0,0]
+
  */
 
 import java.util.Arrays;
@@ -12,10 +39,9 @@ import java.util.stream.Collectors;
 
 public class HowManyNumbersAreSmallerThanTheCurrentNumber_1365 {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        Map<Integer, Long> map = new TreeMap<>();
-        map = Arrays.stream(nums)
+        TreeMap<Integer, Long> map = Arrays.stream(nums)
                 .boxed()
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+                .collect(Collectors.groupingBy(Function.identity(), TreeMap::new, Collectors.counting()));
         long count = 0;
         for (Map.Entry<Integer, Long> entry : map.entrySet()) {
             long temp = entry.getValue();
