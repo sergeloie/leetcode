c26f35417c50cf82ac4d267a8d7abf6d3846692f
serge loie <serge.loie@yandex.ru>
2024-08-05 16:08:33 +0500

    977. Squares of a Sorted Array solved

---
+++ two_pointers/SquaresOfASortedArray_977.java
@@ -0,0 +1,39 @@
+package two_pointers;
+
+/*
+https://leetcode.com/problems/squares-of-a-sorted-array/description/
+977. Squares of a Sorted Array
+Easy
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+
+
+Example 1:
+
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+Example 2:
+
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+ */
+
+public class SquaresOfASortedArray_977 {
+    public int[] sortedSquares(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int[] result = new int[nums.length];
+        for (int i = nums.length - 1; i >= 0 ; i--) {
+            if (Math.abs(nums[left]) >= Math.abs(nums[right])) {
+                result[i] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[i] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        return result;
+    }
+}
