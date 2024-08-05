d5965d49aa5eeb1baacf155214e398336c8a01d5
serge loie <serge.loie@yandex.ru>
2024-08-05 17:07:45 +0500

    Remove Duplicates from Sorted Array solved

--- Main.java
+++ Main.java
@@ -1,14 +1,16 @@
 import two_pointers.FindFirstPalindromicStringInTheArray_2108;
 import two_pointers.MoveZeroes_283;
+import two_pointers.RemoveDuplicatesFromSortedArray_26;
 import two_pointers.ReverseVowelsOfAString_345;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        MoveZeroes_283 cls = new MoveZeroes_283();
-        int[] arr = {0};
-        cls.moveZeroes(arr);
+        RemoveDuplicatesFromSortedArray_26 cls = new RemoveDuplicatesFromSortedArray_26();
+        int[] arr = {1};
+        int result = cls.removeDuplicates(arr);
+        System.out.println(result);
         System.out.println((Arrays.toString(arr)));
     }
 }
--- two_pointers/MoveZeroes_283.java
+++ two_pointers/MoveZeroes_283.java
@@ -11,4 +11,22 @@ public class MoveZeroes_283 {
             }
         }
     }
+
+    public void moveZeroes2(int[] nums) {
+        int zero = 0;
+        int notZero = 0;
+        while (zero < nums.length && notZero < nums.length) {
+            while (zero < nums.length && nums[zero] != 0) {
+                zero++;
+            }
+            notZero = zero;
+            while (notZero < nums.length && nums[notZero] == 0) {
+                notZero++;
+            }
+            if (zero < nums.length && notZero < nums.length) {
+                nums[zero] = nums[notZero];
+                nums[notZero] = 0;
+            }
+        }
+    }
 }
