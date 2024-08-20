78a0011f3b0189ceb882aab7db1d7686e691d4f4
daBoss <serge.loie@yandex.ru>
2024-08-20 20:57:00 +0500

    2529. Maximum Count of Positive Integer and Negative Integer solved

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import binary_search.BinarySearch_704;
 import binary_search.FirstBadVersion_278;
 import binary_search.GuessNumberHigherOrLower_374;
+import binary_search.MaximumCountOfPositiveIntegerAndNegativeInteger_2529;
 import binary_search.SearchInsertPosition_35;
 import two_pointers.FindFirstPalindromicStringInTheArray_2108;
 import two_pointers.MoveZeroes_283;
@@ -12,10 +13,12 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        SearchInsertPosition_35 cls = new SearchInsertPosition_35();
-        int[] nums = {1,3,5,6};
+        MaximumCountOfPositiveIntegerAndNegativeInteger_2529 cls = new MaximumCountOfPositiveIntegerAndNegativeInteger_2529();
+        int[] nums = {-2,-1,-1,1,2,3};
+        int[] nums2 = {5,20,66,1314};
+        int[] nums3 = {-3,-2,-1,0,0,1,2};
         int target = -1;
-        int result = cls.searchInsert(nums, target);
+        int result = cls.maximumCount(nums3);
         System.out.println(result);
     }
 }
