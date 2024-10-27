6128a242ffb6e3263a1cd64d736f37be847dfa29
daBoss <serge.loie@yandex.ru>
2024-10-27 12:17:46 +0500

    739. Daily Temperatures solved

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import recursion.PowerOfThree_326;
 import stack.DailyTemperatures_739;
 import stack.ValidParentheses_20;
 
@@ -5,10 +6,9 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        DailyTemperatures_739 cls = new DailyTemperatures_739();
+        PowerOfThree_326 cls = new PowerOfThree_326();
         int[] arr = new int[]{73,74,75,71,69,72,76,73};
-        System.out.println(Arrays.toString(cls.dailyTemperatures(arr)));
-
+        System.out.println(cls.isPowerOfThree(0));
     }
 
 
